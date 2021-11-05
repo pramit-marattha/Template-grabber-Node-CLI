34e16f03670503027a53c9f512c6f729a3b9db02
pramit-marattha <pramitmarattha5497@gmail.com>
2021-11-05 13:50:53 +0545

    project templated added

--- src/main.js
+++ src/main.js
@@ -9,3 +9,40 @@ const access = promisify(fs.access);
 
 // reccusive copy
 const copy = promisify(ncp);
+
+// async function to copy template files
+async function copyProjectTemplateFiles(opts) {
+  return copy(opts.templateDirectory, opts.targetDirectory, {
+    clobber: false,
+  });
+}
+
+export async function  (opts) {
+  opts = {
+    ...opts,
+    targetDirectory: opts.targetDirectory || process.cwd(),
+  };
+
+  const fullPathName = new URL(import.meta.url).pathname;
+  let templateDir = path.resolve(
+    fullPathName.substr(fullPathName.indexOf("/")),
+    "../../projectTemplates",
+    opts.template.toLowerCase()
+  );
+  templateDir = templateDir.substring(3);
+  opts.templateDirectory = templateDir;
+
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    console.log(chalk.red(`Template directory ${templateDir} does not exist`));
+    console.log(err);
+    process.exit(1);
+  }
+
+  // console.log("Copy project files");
+  // await copyProjectTemplateFiles(opts);
+
+  console.log(chalk.green(`Creating project from template ${opts.template}`));
+  return true;
+}
